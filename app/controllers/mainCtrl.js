23228d4299dc6f7b4a616d39bb7a3afbfaae0631
Dominika <d.k.krzysztofik@gmail.com>
2016-11-14 18:08:10 +0100

    Initial commit

---
+++ app/controllers/mainCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Dominika on 2016-11-07.
+ */
+angular.module('myApp')
+    .controller('mainCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+        $scope.basic = "Basic view";
+    }]);
